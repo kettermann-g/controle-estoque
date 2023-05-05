ec3d0dffe581b620f31a2906d9c79bd40d1658dc
Richard Aydan Fiuza Ost <122907674+aydanfiuza@users.noreply.github.com>
2023-05-05 18:14:59 -0300

    ai ai

--- movimentacao.php
+++ movimentacao.php
@@ -8,15 +8,22 @@
     <main class="container">
         <div class="container-form" id="container-form">
             <form action="cadastro.php" method="post" id="formulario">
-                <h1 class="h1-cadastro">Cadastro</h1>
+                <h1 class="h1-cadastro">Movimentação de estoque</h1>
             <div class="form-inputs-container">
-                <input class="input" id="referencia" name="referencia" required="required" type="number" max="999999" placeholder="Referência do produto"/>
+                <input class="input" id="referencia" name="referencia" required="required" type="number" max="999999" placeholder="Nota fiscal"/>
 
-                <input class="input" id="produto" name="produto" required="required" type="text" placeholder="Nome do produto"/>
+                <input class="input" id="origem" name="origem" required="required" type="text" placeholder="Origem ou destino"/>
 
-                <input class="input" id="valor_unitario" name="valor_unitario" required="required" type="number" max="999999" placeholder="Valor unitário"/>
-            
-                <input class="input" id="descricao" name="descricao" required="required" type="text" placeholder="Descrição do produto"/>
+                <div class="radio-container">
+                    <input type="radio" id="huey" name="movi" value="huey"
+                            checked>
+                    <label for="huey">Entrada</label>
+                </div>
+
+                <div class="radio-container">
+                    <input type="radio" id="saida" name="movi" value="dewey">
+                    <label for="dewey">Saída</label>
+                </div>
 
                 <input type="submit" id="submit-button"></input>
             </div>
