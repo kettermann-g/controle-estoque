978875941d99f0f96c511530c4f48822187fa6db
baunirashaek <gabriel.kettermann@gmail.com>
2023-05-31 19:46:12 -0300

    tentando confirmar senha

---
+++ js/confirmar-senhas.js
@@ -0,0 +1,8 @@
+
+function confirmarSenhas() {
+    senha = $
+    $("#confirmar-senha-text").on( "keypress", function() {
+        var bla = $("#confirmar-senha-text").val();
+        console.log(bla);
+      } );
+}
