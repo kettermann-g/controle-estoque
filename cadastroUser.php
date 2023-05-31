93f42dc07e77b119f62a54a9c189fd853b900760
baunirashaek <gabriel.kettermann@gmail.com>
2023-05-31 20:05:02 -0300

    testando senhas iguais funcionando

--- cadastroUser.php
+++ cadastroUser.php
@@ -13,11 +13,11 @@
   <title>Cadastrar</title>
 
   <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
-  <link rel="stylesheet" href="<?= $BASE_URL ?>/css/responsive.css">
   <link rel="stylesheet" href="<?= $BASE_URL ?>/css/stylesLogin.css">
 
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
   
+  <script src="<?= $BASE_URL ?>/js/jQuery/jquery-3.7.0.js"></script>
 </head>
 <body>
     
@@ -46,14 +46,19 @@
           <input id="email" name="email" type="email" placeholder="Digite seu e-mail">
         </div>
 
-        <div class="input-field">
+        <div class="input-field" id="senha-cad">
           <label class="label-form" for="senha" >Senha:</label>
           <input id="senha" name="senha" type="password" required="required" placeholder="Senha">
         </div>
 
-        <div class="input-field">
+        <div class="input-field" id="conf-senha-cad">
           <label class="label-form" for="confirmar-senha" >Confirme a senha:</label>
-          <input id="confirmar-senha" name="confirmar-senha" type="password" required="required" placeholder="Confirme aqui a sua senha">
+          <input id="confirmar-senha-text" name="confirmar-senha" type="password" required="required" placeholder="Confirme aqui a sua senha">
+        </div>
+
+        <div>
+          <p id="conf-senha-texto" class="senhas-nao-coincidem"><i id="id-i-senha-igual" class="fa-solid fa-circle-xmark"></i> As senhas precisam ser iguais</p>
+          <p id="caracteres-senha" class="senhas-nao-coincidem"><i id="id-i-senha-caracteres" class="fa-solid fa-circle-xmark"></i> A senha precisa ter 8 caracteres ou mais</p>
         </div>
 
         <div class="input-field">
@@ -66,11 +71,10 @@
       </form>
     </div>
   </div>
-  <script src="styleLogin.js"></script>
-
   
   <script src="https://kit.fontawesome.com/5fda86d671.js" crossorigin="anonymous"></script>
-  <script src="<?= $BASE_URL ?>/js/index.js"></script>
+  <script src="<?= $BASE_URL ?>/js/confirmar-senhas.js"></script>
+  
 
 </body>
 </html>
--- js/confirmar-senhas.js
+++ js/confirmar-senhas.js
@@ -1,8 +1,26 @@
 
 function confirmarSenhas() {
-    senha = $
-    $("#confirmar-senha-text").on( "keypress", function() {
-        var bla = $("#confirmar-senha-text").val();
-        console.log(bla);
-      } );
+    var confSenha = $("#confirmar-senha-text").val();
+    var senha = $("#senha").val();
+
+    console.log("SENHA: " + senha);
+    console.log("CONF SENHA: " + confSenha);
+
+    if (confSenha === senha) {
+        $("#conf-senha-texto").removeClass("senhas-nao-coincidem");
+        $("#conf-senha-texto").addClass("senhas-coincidem");
+
+        $("#id-i-senha-igual").removeClass("fa-circle-xmark");
+        $("#id-i-senha-igual").addClass("fa-circle-check");
+
+    } else {
+        $("#conf-senha-texto").addClass("senhas-nao-coincidem");
+        $("#conf-senha-texto").removeClass("senhas-coincidem");
+
+        $("#id-i-senha-igual").addClass("fa-circle-xmark");
+        $("#id-i-senha-igual").removeClass("fa-circle-check");
+    }
 }
+
+$("#confirmar-senha-text").keyup(confirmarSenhas);
+$("#senha").keyup(confirmarSenhas);
